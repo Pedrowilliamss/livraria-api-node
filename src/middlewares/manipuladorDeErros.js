3cd580b27c62be2f62ea01d7db1c8b3894d530f2
Pedro Williams <pedrowilliamsbc@gmail.com>
2023-04-22 13:59:42 -0300

    fix

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
+import ErroBase from "../erros/ErroBase.js";
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
+import ErroValidacao from "../erros/ErroValidacao.js";
 
 // eslint-disable-next-line no-unused-vars
 function manipuladorDeErros (erro, req, res, next) {
   if (erro instanceof mongoose.Error.CastError) {
-    res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos."});
+    new RequisicaoIncorreta().enviarResposta(res);
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    new ErroValidacao(erro).enviarResposta(res);
   } else { 
-    res.status(500).send({message: "Erro interno de servidor"});
+    new ErroBase().enviarResposta(res);
   }
 }
 
